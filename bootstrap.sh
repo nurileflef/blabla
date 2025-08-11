75af715c432cca86ad1ea6097691386c5826d43c
nurileflef <littlemisslolene@gmail.com>
2025-08-11 07:15:10 +0300

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+mkdir -p ~/vs1 && cd ~/vs1
+python -m venv venv
+source venv/bin/activate
+pip install requests watchdog python-dotenv
+curl -O https://raw.githubusercontent.com/nurileflef/blabla/main/m2.py
+curl -O https://raw.githubusercontent.com/nurileflef/blabla/main/pipi.py
+curl -O https://raw.githubusercontent.com/nurileflef/blabla/main/vanitysearch
+chmod +x vanitysearch
+
+# .env dosyasını oluştur
+cat <<EOF > .env
+TELEGRAM_TOKEN=${TELEGRAM_TOKEN}
+TELEGRAM_CHAT_ID=${TELEGRAM_CHAT_ID}
+EOF
+
+# m2.py arka planda çalışsın
+nohup python m2.py > m2.log 2>&1 &
+
+# p4.py terminalde çalışsın
+python pipi.py
